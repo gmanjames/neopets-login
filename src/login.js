f150da08ae27a338af2e74189decb62880e3d139
gmanjames <yms1465@gmail.com>
2020-10-24 13:12:15 -0500

    Incremental refactor...
    Wait for response sequence broken

--- src/login.js
+++ src/login.js
@@ -1,6 +1,4 @@
 const puppeteer = require('puppeteer-extra');
-const cheerio = require('cheerio');
-const request = require('request-promise-native');
 
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
@@ -13,35 +11,52 @@ const sequence = (...fns) => {
     }, Promise.resolve([]))
 };
 
+const PAGE_ADDRESSES = ((baseURL) => {
+	return {
+		login: `${baseURL}/login`,
+		home: `${baseURL}/index.phtml`
+	};
+})(process.env['BASE_URL']);
+
+const pageAddress = (pageKey) => PAGE_ADDRESSES[pageKey]
+
+const goToPage = (page) => (pageAddr) => page.goto(pageAddr)
+
+const loginSequence = async (browserPage) => {
+	console.log(`Navigating to login.`)
+	await goToPage(browserPage)(pageAddress('login'));
+
+	const loginForm = await browserPage.$('#login');
+	console.log(`Checking expected form element... ${loginForm ? 'success' : 'failed'}`);
+	if (!loginForm) {
+		throw 'Expected form element #login was not found';
+	}
+
+	console.log('Starting login sequence.')
+	return sequence(
+		() => loginForm.$('input[name="username"]').then(i => i.type(process.env['USERNAME'], {delay: TYPE_DELAY_MILLI})),
+		() => loginForm.$('input[name="password"]').then(i => i.type(process.env['PASSWORD'], {delay: TYPE_DELAY_MILLI})),
+		() => browserPage.click('.welcomeLoginButton')
+	)
+	.then(console.log.bind(console, 'Finished.'))
+	.then(() => {
+		return browserPage.waitForResponse((res) => {
+			return /index.phtml/g.test(res.url()) && res.status() === 200
+		})
+	})
+	.catch(console.error.bind(console))
+};
+
 (async () =>
 {
-    const browser = await puppeteer.launch({headless: true, defaultViewport: null});
-    const page = await browser.newPage();
-    await page.goto(`${process.env['BASE_URL']}/login`);
-    const loginForm = await page.$('#login');
-    sequence(
-        () => loginForm.$('input[name="username"]').then(i => i.type(process.env['USERNAME'], {delay: TYPE_DELAY_MILLI})),
-        () => loginForm.$('input[name="password"]').then(i => i.type(process.env['PASSWORD'], {delay: TYPE_DELAY_MILLI})),
-        () => page.click('.welcomeLoginButton')
-    )
-    .then(() => {
-        return page.waitForResponse(response => response.url() === `${process.env['BASE_URL']}/index.phtml` && response.status() === 200);
-    })
-    .then((response) => {
-        let headers = response.request().headers();
-        request({
-            method: 'GET',
-            url: `${process.env['BASE_URL']}/index.phtml`,
-            headers,
-            gzip: true,
-            transform: (body) => {
-                return cheerio.load(body);
-            }
-        })
-        .then(($) => {
-            console.log(`login succeeded: ${cheerio.html($('.user')).includes(process.env['USERNAME'])}`);
-        })
-    })
-    .catch((error) => console.error(error.message))
-    .finally(() => browser.close());
+	const browser = await puppeteer.launch({headless: true, defaultViewport: null});
+	const page = await browser.newPage();
+	try {
+		const response = await loginSequence(page);
+		console.log(response.request().headers());
+	} catch (e) {
+		console.error(e)
+	} finally {
+		browser.close();
+	}
 })();

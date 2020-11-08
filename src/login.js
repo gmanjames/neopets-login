16ee21e447af0ff8a4f8fa5ae0e4adb8c1e318ff
gmanjames <yms1465@gmail.com>
2020-11-08 12:26:11 -0600

    Add timeout for operation

--- src/login.js
+++ src/login.js
@@ -1,7 +1,14 @@
-const puppeteer = require('puppeteer-extra');
+const baseURL = process.env['BASE_URL'],
+	  username = process.env['NEOPETS_USERNAME'],
+	  password = process.env['NEOPETS_PASSWORD'],
+	  dataDir  = process.env['DATA_DIRECTORY'];
 
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+const puppeteer = require('puppeteer-extra'),
+	  StealthPlugin = require('puppeteer-extra-plugin-stealth'),
+	  AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
+
 puppeteer.use(StealthPlugin())
+puppeteer.use(AdblockerPlugin())
 
 const TYPE_DELAY_MILLI = 300;
 
@@ -11,20 +18,11 @@ const sequence = (...fns) => {
     }, Promise.resolve([]))
 };
 
-const PAGE_ADDRESSES = ((baseURL) => {
-	return {
-		login: `${baseURL}/login`,
-		home: `${baseURL}/index.phtml`
-	};
-})(process.env['BASE_URL']);
-
-const pageAddress = (pageKey) => PAGE_ADDRESSES[pageKey]
-
 const goToPage = (page) => (pageAddr) => page.goto(pageAddr)
 
 const loginSequence = async (browserPage) => {
 	console.log(`Navigating to login.`)
-	await goToPage(browserPage)(pageAddress('login'));
+	await goToPage(browserPage)(`${baseURL}/login`);
 
 	const loginForm = await browserPage.$('#login');
 	console.log(`Checking expected form element... ${loginForm ? 'success' : 'failed'}`);
@@ -34,29 +32,46 @@ const loginSequence = async (browserPage) => {
 
 	console.log('Starting login sequence.')
 	return sequence(
-		() => loginForm.$('input[name="username"]').then(i => i.type(process.env['USERNAME'], {delay: TYPE_DELAY_MILLI})),
-		() => loginForm.$('input[name="password"]').then(i => i.type(process.env['PASSWORD'], {delay: TYPE_DELAY_MILLI})),
+		() => loginForm.$('input[name="username"]').then(i => i.type(username, {delay: TYPE_DELAY_MILLI})),
+		() => loginForm.$('input[name="password"]').then(i => i.type(password, {delay: TYPE_DELAY_MILLI})),
 		() => browserPage.click('.welcomeLoginButton')
 	)
 	.then(console.log.bind(console, 'Finished.'))
-	.then(() => {
-		return browserPage.waitForResponse((res) => {
-			return /index.phtml/g.test(res.url()) && res.status() === 200
-		})
-	})
 	.catch(console.error.bind(console))
+};
+
+const withTimeout = (fn, timeout, cb) => {
+	let wait = fn.call();
+	Promise.race([
+		wait,
+		new Promise((_, reject) => {
+			setTimeout(reject.bind(null, 'Timeout exceeded.'), timeout * 1000);
+		})
+	])
+	.finally(cb)
 };
 
 (async () =>
 {
-	const browser = await puppeteer.launch({headless: true, defaultViewport: null});
+	const browser = await puppeteer.launch({headless: false, defaultViewport: null});
 	const page = await browser.newPage();
-	try {
-		const response = await loginSequence(page);
-		console.log(response.request().headers());
-	} catch (e) {
-		console.error(e)
-	} finally {
-		browser.close();
-	}
+
+	page.on('response', async (response) => {
+		let status       = response.status(),
+			url          = response.url(),
+			targetURL    = `${baseURL}/login.phtml`
+			targetStatus = 302;
+
+		console.log(`Received ${status} from ${url.slice(0, 50)}...`);
+
+		if (url === targetURL && status === targetStatus) {
+			console.log(`Target URL reached, checking cookies...`);
+			let setCookieHeader = response.headers()['set-cookie'] || '';
+			// TODO: Save to DATA_DIR
+			console.log({setCookieHeader});
+			browser.close()
+		}
+	});
+
+	withTimeout(loginSequence.bind(null, page), 60, () => browser.close()); // in seconds
 })();
